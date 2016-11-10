2d446f53387ac881283cf4d12c5b6d02c94f8f69
Daniel Payne <daniel@havenagency.com>
2016-11-10 12:51:08 -0800

    initial comit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+exports.localeReplace = function (localeRegions,locale) {
+    var localeRegions ={
+                                        'en-US': 'us',
+                                        'pt-BR': 'us',
+                                        'es-MX': 'us',
+                                        'ko-KR': 'kr',
+                                        'zh-TW': 'tw',
+                                        'zh-CN': 'cn',
+                                        'de-DE': 'eu',
+                                        'es-ES': 'eu',
+                                        'fr-FR': 'eu',
+                                        'it-IT': 'eu',
+                                        'pl-PL': 'eu',
+                                        'ja-JP': 'us',
+                                        'ru-RU': 'eu'
+                                    };
+
+    var normalizedLocale = locale;
+    var region, separator, language;
+
+    //  Matches if a string is two lowercase or uppercase letters, followed by an underscore or hyphen, followed by two lowercase or uppercase letters.
+    const regexLoose = /^[a-zA-Z]{2}[-_][a-zA-Z]{2}$/;
+
+    //  Matches if a string is two lowercase letters, followed by a hyphen, followed by two uppercase letters
+    const regexStrict = /^[a-z]{2}[-][A-Z]{2}$/;
+
+    if (!regexLoose.test(locale)) {
+        console.warn("WARNING: locale should be in the following format: 'en-US' (two lowercase letters, hyphen, two uppercase letters).");
+        //return locale;
+    }
+
+    if (!regexStrict.test(locale)) {
+        region = locale.substr(0,2).toLowerCase();
+        separator = "-";
+        language = locale.substr(3,4).toUpperCase();
+        normalizedLocale = region + separator + language;
+    }
+
+    return [normalizedLocale, localeRegions[normalizedLocale]];
+
+}
